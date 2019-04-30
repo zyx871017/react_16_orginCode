6cd9f64700755718da3338486c9af2aa400ec09a
zhaoyuxiang <112408584@qq.com>
2019-04-30 14:18:38 +0800

    comment react

--- app.js
+++ app.js
@@ -4,20 +4,39 @@ import react from 'react';
 class MyDom extends react.Component {
     constructor(props) {
         super(props);
+        this.state = {text: '111'}
     }
 
     componentDidMount() {
         setTimeout(() => {
-            this.forceUpdate();
+            this.setState({text: '222'})
         }, 5000);
     }
 
     render() {
-        return react.createElement('div', {style: {width: 100, height: 100, border: '1px red solid'}});
+        return react.createElement('div', {style: {width: 100, height: 100, border: '1px red solid'}}, this.state.text);
     }
 }
 
 
-const dom = react.createElement(MyDom);
+class MySecDom extends react.Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: '333'};
+    }
+
+    render() {
+        return react.createElement('div', {
+            style: {
+                width: 200,
+                height: 200,
+                border: '1px green solid'
+            }
+        }, react.createElement(MyDom));
+    }
+}
+
+
+const dom = react.createElement(MySecDom);
 
 reactDOM.render(dom, document.getElementById('root'));
